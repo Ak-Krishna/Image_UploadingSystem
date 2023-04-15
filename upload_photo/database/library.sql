40a746dbadad9f9d07a1da7846bc87bc81f5e777
Kishan_Aher <79781138+Ak-Krishna@users.noreply.github.com>
2023-04-15 15:28:03 +0530

    Uploaded Sql file to create database and table

---
+++ upload_photo/database/library.sql
@@ -0,0 +1,25 @@
+-- database creation --
+-- CREATE DATABASE upload_images;
+
+-- use the database --
+-- USE upload_images;
+
+-- schema for the user Data table--
+-- CREATE TABLE user_data(
+--     User_id INT NOT NULL AUTO_INCREMENT,
+--     username VARCHAR(30) NOT NULL,
+--     emailId VARCHAR(60) NOT NULL UNIQUE,
+--     password VARCHAR(42) NOT NULL,
+--     user_Image VARCHAR(60),
+--     date DATE DEFAULT CURRENT_TIMESTAMP
+-- )
+
+-- schema for Images table
+-- CREATE TABLE images(
+--     name  VARCHAR(80) NOT NULL UNIQUE,
+--     size VARCHAR(60) NOT NULL,
+--     image VARCHAR(70),
+--     thumbnail VARCHAR(80)
+--     date DATE DEFAULT CURRENT_TIMESTAMP
+-- )
+
